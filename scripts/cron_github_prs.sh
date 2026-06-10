2a33210df166fe58d74137cc610a965f67bafd6f
Matthieu Muffato <mm49@sanger.ac.uk>
2026-06-10 11:14:17 +0100

    Only report on the last week

--- scripts/cron_github_prs.sh
+++ scripts/cron_github_prs.sh
@@ -4,4 +4,4 @@ source /software/treeoflife/etc/slack_webhooks_env.sh
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 
-exec env "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_TOL_PIPELINES}" "${script_dir}/github_prs.py"
+exec env "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_TOL_PIPELINES}" WEEKS=1 "${script_dir}/github_prs.py"
