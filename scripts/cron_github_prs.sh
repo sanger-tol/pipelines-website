928f5773323404d803ac23538c477d0f1e6d7ab3
Matthieu Muffato <mm49@sanger.ac.uk>
2026-06-10 11:10:52 +0100

    With the cron job

---
+++ scripts/cron_github_prs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+source /software/treeoflife/etc/slack_webhooks_env.sh
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+
+exec env "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_TOL_PIPELINES}" "${script_dir}/github_prs.py"
